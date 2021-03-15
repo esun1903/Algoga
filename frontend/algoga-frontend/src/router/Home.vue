4b002d659d7436da72db72a10d78e90205352468
multicampus <pica1217@naver.com>
2021-03-15 17:56:44 +0900

    [S04P22A302-28] feat/park/SettingComponents : Update Components

---
+++ frontend/algoga-frontend/src/router/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='home'> 
+      <HomeNavbar />
+      <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+import HomeNavbar from '@/components/Home/HomeNavbar'
+
+
+export default {
+    name : 'Home',
+    components : {
+      HomeNavbar,
+    },
+}
+</script>
+
+<style>
+
+</style>
