e3e477f978c4d262d0affe36c7c3adb6a7573533
이동희 <ldh297@naver.com>
2021-03-16 14:40:54 +0900

    passCode

--- frontend/algoga-frontend/src/components/Home/HomeNavbar.vue
+++ frontend/algoga-frontend/src/components/Home/HomeNavbar.vue
@@ -1,6 +1,19 @@
 <template>
   <div id='home-navbar'>
-    <h1>homenavbar</h1>
+    <nav>
+      <div>
+
+      </div>
+      <div>
+        <span>무엇이</span>
+        <span>들어갈지</span>
+        <span>정해봅시다</span>
+
+      </div>
+      <div>
+        
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -11,5 +24,23 @@ export default {
 </script>
 
 <style>
+
+#home-navbar {    
+  position:sticky;
+  top: 0;
+  width: 100vw;
+  height: 80px;   
+  background-color: var(--test-color);
+}
+
+#home-navbar > nav {
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#home-navbar > nav > div:nth-child(1) {
+  background-image: "../../assets/2.jpg";
+}
 
 </style>
