4f137bf09e3425e09a6e01d63c807adf9b5dcc3b
이동희 <ldh297@naver.com>
2021-03-16 14:40:54 +0900

    passCode

---
+++ frontend/README.md
@@ -0,0 +1,14 @@
+# Algoga-Frontend
+
+
+
+## installed
+
+``` bash
+$ npm install vue-router --save
+$ npm install vuex --save
+$ npm install vuex-persistedstate --save
+$ npm install lodash --save
+$ npm install axios --save
+```
+
--- frontend/algoga-frontend/src/App.vue
+++ frontend/algoga-frontend/src/App.vue
@@ -16,12 +16,19 @@ export default {
 </script>
 
 <style>
+:root {
+  --font-color: black;
+  --back-color: white;
+  --test-color: red;
+
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50;  
 }
 </style>
