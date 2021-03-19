2ba7372aa8f270d6705f4de5a6c5e63bbe6ed132
esun1903 <dmstjs1996@naver.com>
2021-03-19 09:44:17 +0900

    update login

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -6,5 +6,5 @@ class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('user_email', 'userr_baek_id', 'user_password','user_nickname','user_register_date')
+        fields = ('user_email', 'user_baek_id', 'user_password','user_nickname')
          # 이메일, 비밀번호, 백준 아이디, 닉네임, 프로필이미지(기본) , 회원가입날짜
