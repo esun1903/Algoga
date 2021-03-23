e85c36b4757009414697c4528d8d0dbcea5f5186
esun1903 <dmstjs1996@naver.com>
2021-03-23 16:49:59 +0900

    [S04P23A302-49] feat/choi/LogoutUpdateUser/insert 회원탈퇴

--- backend2/backend/apps/urls.py
+++ backend2/backend/apps/urls.py
@@ -3,6 +3,16 @@ from django.conf import settings
 from .views import viewSet
 
 urlpatterns = [ 
+
+
+    #회원가입
     path("v1/user", viewSet.as_view({"get": "list", "post": "add"}), name="user"),
     path("v1/user/<int:user_int>", viewSet.as_view({"get": "list"}), name="user"),
+
+    #회원수정
+    path("v1/userupdate?", viewSet.as_view({"post" : "update"}), name="userSeq"),
+
+    #회원탈퇴
+    path("v1/userdelete/<int:user_seq>", viewSet.as_view({"get": "Userdelete"}), name="user_seq"),
+    
 ]
