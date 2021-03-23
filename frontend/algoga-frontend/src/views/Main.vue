fcd1e0c965a4738e2cb7d63edc31c86ef4b7b481
이동희 <ldh297@naver.com>
2021-03-23 16:25:17 +0900

    [S04P23A302-15,S04P23A302-14] feat/lee/MainStudyHistory:Create StudyLog and History

--- frontend/algoga-frontend/src/views/Main.vue
+++ frontend/algoga-frontend/src/views/Main.vue
@@ -1,16 +1,52 @@
 <template>
   <div id="main">
+    <MainNavbar />
+
+    <div>
+      <div>
+        <!-- feed -->
+      </div>
+      <section>
+        
+        <!-- challenge -->
+        here / challenge components 
+
+        <LogStudy />
+        <!-- history -->
+
+        <History />
+      </section>
+      <div>
+        <!-- 여기는 status가들어가겠조? -->
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
+import MainNavbar from "@/components/Main/MainNavbar"
+import LogStudy from "@/components/Main/LogStudy"
+import History from "@/components/Main/History"
+
+
 export default {
-    name:"Main",
+  name:"Main",
+  components: {
+      MainNavbar,
+      LogStudy,
+      History,
+  }
 
 }
 </script>
 
 <style>
+#main > div >section {
+  width: 1000px;    
+  margin: 20px auto;
+    
+}
+
 
 </style>
