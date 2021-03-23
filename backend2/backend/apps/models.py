8321d25a11900d8fc6f1c76141663e98cdb6463e
pyoki32 <pyoki32@gmail.com>
2021-03-24 00:58:47 +0900

    [S04p23A302-22] feat/pyoki/signUp create signUp

--- backend2/backend/apps/models.py
+++ backend2/backend/apps/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_seq = models.AutoField(primary_key=True)
-    user_email = models.CharField(max_length=50)
-    user_password = models.CharField(max_length=50)
-    user_baek_id = models.CharField(max_length=50, blank=True, null=True)
-    user_nickname = models.CharField(max_length=50)
-    user_profile_image = models.CharField(max_length=500)
-    user_register_date = models.DateTimeField()
+    seq = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    baek_id = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=500, blank=True, null=True)
+    register_date = models.DateTimeField()
 
     class Meta:
         managed = False
--- backend2/backend/apps/urls.py
+++ backend2/backend/apps/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path 
 from django.conf import settings 
-from .views import viewSet
+from .views import *
 
 urlpatterns = [ 
 
 
     #회원가입
-    path("v1/user", viewSet.as_view({"get": "list", "post": "add"}), name="user"),
-    path("v1/user/<int:user_int>", viewSet.as_view({"get": "list"}), name="user"),
+    path("v1/signUp", UserViewSet.as_view({"post": "signUp"})),
 
     #회원수정
     path("v1/userupdate?", viewSet.as_view({"put" : "update"}), name="userSeq"),
@@ -16,6 +15,8 @@ urlpatterns = [
     path("v1/userdelete/<int:user_seq>", viewSet.as_view({"get": "Userdelete"}), name="user_seq"),
 
     #로그아웃
-    path('logout/', LogoutView.as_view(), name='auth_logout'),
+   # path('logout/', LogoutView.as_view(), name='auth_logout'),
+
+
     
 ]
