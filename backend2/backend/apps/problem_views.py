59266bba6eeecd711be2d40a07d9b909bc303b72
pyoki32 <pyoki32@gmail.com>
2021-03-25 09:52:31 +0900

    feat/devleop backend code 수정
    Former-commit-id: 6ffc607e2e1ef601c1f62aa37dcaa07efe0e2051

--- backend2/backend/apps/problem_views.py
+++ backend2/backend/apps/problem_views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from django.views import View 
 from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 class viewSet(viewsets.ModelViewSet):
@@ -44,19 +44,6 @@ class viewSet(viewsets.ModelViewSet):
 
 
 
-#로그아웃을 위한 api
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
       
