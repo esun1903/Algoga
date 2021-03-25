f1b57cf92533710dc6fc667f0d4dfe7d472ce92a
이동희 <ldh29768@gmail.com>
2021-03-25 17:26:20 +0900

    [S04P23A302-57] feat/lee/SolvingProb: Create/RegisterPage
    Former-commit-id: ce5561edde02d1ccfd9329994d0c01dd9168ac5d

--- frontend/algoga-frontend/src/main.js
+++ frontend/algoga-frontend/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Codemirror from 'vue-codemirror'
+import "codemirror/lib/codemirror.css"
 
 Vue.config.productionTip = false
+
+Vue.use(Codemirror)
 
 new Vue({
   store,
