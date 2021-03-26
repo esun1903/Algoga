9a912847b44cf4b16ec78615f397c239dd0d7ebe
pyoki32 <pyoki32@gmail.com>
2021-03-26 15:50:18 +0900

    [S04P23A302-27] feat/pyoki/codeBoard create regite
    Former-commit-id: 63bd87fe28accd86a9199cea09c07ed556dd5047

--- backend2/backend/apps/urls.py
+++ backend2/backend/apps/urls.py
@@ -7,21 +7,21 @@ from . import views
 urlpatterns = [ 
     #로그인
     path("v1/login/<str:email>/<str:password>", UserViewSet.as_view({"get": "login"})),
-
     #로그아웃
-    path('v1/logout/', UserViewSet.as_view({"get": "logout"})),
-
+    path('v1/logout', UserViewSet.as_view({"get": "logout"})),
     #세션확인
-    path('v1/sessionCheck/', UserViewSet.as_view({"get": "sessionCheck"})),
-
+    path('v1/sessionCheck', UserViewSet.as_view({"get": "sessionCheck"})),
     #회원가입
     path("v1/signUp", UserViewSet.as_view({"post": "signUp"})),
-
     #회원수정
     path("v1/userInfoUpdate/<str:email>", UserViewSet.as_view({"put": "userInfoUpdate"})),
-
     #회원탈퇴
     path("v1/userdelete/<str:email>", UserViewSet.as_view({"delete": "Userdelete"})),
+    #코드풀이 등록
+    path("v1/codeBoardRegiste", codeBoardViewSet.as_view({"post": "codeBoardRegiste"})),
+    
+
+
 
     #문제추천해주기 
     path("v1/problem", viewSet.as_view({"put" : "update"}), name="userSeq"),
