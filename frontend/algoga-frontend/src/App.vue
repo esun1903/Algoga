b0b8e70d63bbaf56c8b837b02a2bb185e84f8472
이동희 <ldh29768@gmail.com>
2021-03-28 20:56:17 +0900

    [S04P23A302-70] feat/lee/HomeDesign: Update/HomeDesign
    Former-commit-id: 2f561b80605231b73973b72e6142cea435faa042

--- frontend/algoga-frontend/src/App.vue
+++ frontend/algoga-frontend/src/App.vue
@@ -13,28 +13,72 @@ export default {
   name: 'App',
   components: {
     ModeBtn,
+  },
+  created(){
+    window.addEventListener('click',function(event){
+      let d = document.createElement('div')
+      d.className = 'clickEffect'
+      d.style.top = event.clientY + 'px'; d.style.left = event.clientX+'px';
+      d.addEventListener('animationend',function(){
+          d.parentElement.removeChild(d)
+        })
+      document.body.appendChild(d)
+
+    })
   }
+
 }
 </script>
 
 <style>
 :root {
   --font-color: black;
+  --font-color2:white;
   --back-color: white;
+
   --shadow-color: rgba(0,0,0,0.5);
   --test-color: red;
+  --logo-color: rgb(252, 69, 69);
+}
 
+@keyframes clickEffect{
+  0%{
+    opacity:1;
+    width:0.5em; height:0.5em;
+    margin: -0.25em;
+    border-width: 0.5em;
+  }
+  100%{
+    opacity:0.2;
+    width:3em; height:3em;
+    margin: -1.5em;
+    border-width: 0.05em;
+  }
 }
 
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: Hack;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;  
   background-color: var(--back-color);
+  transition: 0.3s; 
+  
 }
 .text-center{
   text-align: center;
 }
+
+.clickEffect {
+  position:fixed;
+  box-sizing: border-box;
+  border-style: solid;
+  border-color: var(--shadow-color);
+  border-radius: 50%;
+  animation:clickEffect 0.4s ease-out;
+  z-index:99999;
+}
+
 </style>
