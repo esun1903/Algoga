a9a743f55c0e3f8ed615cdd4957c7fe5f86541fb
이동희 <ldh29768@gmail.com>
2021-03-28 20:56:17 +0900

    [S04P23A302-70] feat/lee/HomeDesign: Update/HomeDesign
    Former-commit-id: fd3c6726d1b06e88c188df3ddc8a8e0faae92409

--- frontend/algoga-frontend/src/components/Home/HomeNavbar.vue
+++ frontend/algoga-frontend/src/components/Home/HomeNavbar.vue
@@ -2,11 +2,12 @@
   <div id='home-navbar'>
     <nav>
       <div>
-        <img src="../../assets/2.jpg" alt="" height="60px">        
+        <!-- <img src="../../assets/2.jpg" alt="" height="60px">         -->
+        <h1><span>//</span>ALGOGA</h1>
         <div>
           <div>first link</div>
           <div>second link</div>
-          <div>third link</div>
+          <div>third link</div>                   
         </div>
       </div>
       <div>
@@ -21,8 +22,7 @@
 export default {
     name : 'HomeNavbar',
     methods:{
-      emitSign:function(status){
-        console.log('emitSign')
+      emitSign:function(status){        
         if (status === 1) {
           this.$emit("signin")
         } else {
@@ -49,11 +49,14 @@ export default {
 #home-navbar {    
   position:sticky;
   top: 0px;
-  width: 80vw;
+  width: 95%;
   height: 60px;   
-  margin:10px auto 0 auto;
+  margin:0px auto 0 auto;
   background-color: var(--back-color);  
   border-radius: 5px;  
+  color: var(--font-color);
+  z-index: 10000;
+    
 }
 
 .nav-shadow {
@@ -64,18 +67,33 @@ export default {
   display:flex;
   justify-content: space-between;
   align-items: center;
+  width:80%;
+  margin:0 auto;
 }
 
 #home-navbar > nav > div:nth-child(1) {
-  background-image: url("../../assets/2.jpg");
-  background-repeat: no-repeat;
+  /* background-image: url("../../assets/2.jpg"); */
+  /* background-repeat: no-repeat; */
   display:flex;
+  
+}
+
+#home-navbar > nav >div:nth-child(1) > h1 {
+  font-style:italic;  
+  margin-left: 20px;
+
+}
+#home-navbar > nav >div:nth-child(1) > h1 > span {
+  color:var(--logo-color);
+  font-weight: bold;
 }
 
 #home-navbar > nav >div:nth-child(1) > div {  
   display:flex;
   background-color: var(--back-color);
   align-items: center;  
+  height:100%;
+  margin: auto 0;
 }
 
 #home-navbar > nav >div:nth-child(1) > div > div {
