01393509aed57a0f82488a274ca7500c692e2793
multicampus <pica1217@naver.com>
2021-04-04 22:05:56 +0900

    merge
    Former-commit-id: df405be183202dc30ab8482c396a8893f203961d

--- frontend/algoga-frontend/src/store/index.js
+++ frontend/algoga-frontend/src/store/index.js
@@ -1,13 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 import createPersistedState from 'vuex-persistedstate';
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+        algoList: [],
+    },
+    mutations: {
+        getAlgo: function(state, data) {
+            this.state.algoList = data;
+        },
+    },
+    actions: {
+        getAlgo: function(context) {
+            if (this.state.algoList.length === 0) {
+                axios
+                    .get(`${SERVER_URL}/apps/v1/allProblem`)
+                    .then((res) => {
+                        context.commit('getAlgo', res.data);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            } else {
+                context.commit('getAlgo', this.state.algoList);
+            }
+        },
+    },
     getters: {},
     plugins: [createPersistedState()],
 });
