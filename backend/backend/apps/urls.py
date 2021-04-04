d925a9139d36fe8b5dc497e2fc12c29e632549a7
pyoki32 <pyoki32@gmail.com>
2021-04-05 05:55:23 +0900

    [S04P23A302-114]feat/pyokiNickname create findPassword
    Former-commit-id: 8f376b70212006b3c63ae5fa9b28cfe757e8f4b5

--- backend/backend/apps/urls.py
+++ backend/backend/apps/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path("v1/passEmailCheck/<str:email>", UserViewSet.as_view({"get": "passEmailCheck"})),
     #nickname 체크
     path("v1/nicknameCheck/<str:nickname>", UserViewSet.as_view({"get": "nicknameCheck"})),
+    #비밀번호 찾기
+    path("v1/findPassword/<str:email>", UserViewSet.as_view({"get": "findPassword"})),
     #===================================
     #팔로잉걸기 
     path('v1/followUser/<int:user_follower_seq>/<int:user_following_seq>', UserViewSet.as_view({"get": "FollowUser"})),
