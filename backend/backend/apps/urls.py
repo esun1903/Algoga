ce3ba35aeebd5852d60dec5c64ad0954b9f9e515
pyoki32 <pyoki32@gmail.com>
2021-04-04 19:19:18 +0900

    [S04P23A302-30] feat/pyoki/jwtLogin update jwt
    Former-commit-id: 50ba24447e44f19bdb9118a88e26a02205e2992a

--- backend/backend/apps/urls.py
+++ backend/backend/apps/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path("v1/login", UserViewSet.as_view({"post": "login"})),
     #로그아웃
     path('v1/logout', UserViewSet.as_view({"get": "logout"})),
-    #세션확인
-    path('v1/sessionCheck', UserViewSet.as_view({"post": "sessionCheck"})),
+    #로그인토큰검사 재발급
+    path('v1/token_verify', UserViewSet.as_view({"post": "token_verify"})),
     #회원가입
     path("v1/signUp", UserViewSet.as_view({"post": "signUp"})),
     #회원수정
