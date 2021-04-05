00471577b4017da85f066aa9a450a58098a20e17
이동희 <ldh29768@gmail.com>
2021-04-05 15:18:11 +0900

    savepoint
    Former-commit-id: 7665113461eb66d6b64cfa6812d8b755a50d6127

--- frontend/algoga-frontend/src/views/CodeBoardDetail.vue
+++ frontend/algoga-frontend/src/views/CodeBoardDetail.vue
@@ -1,14 +1,23 @@
 <template>
   <div id='code-board-detail'>
     <MainNavbar />
-    <div class='code-board-body'>
+    <div class='code-board-content'>
       <h1>{{title}}</h1>
-      {{data}}
+      <!-- {{data}}
       {{category}}
+      {{userData}}
+      {{registerDay}} -->
       <div class='code-board-detail-status'>
+        <div>
+          Created at {{registerDay[0]}} {{registerDay[1]}}
+        </div>
         
       </div>
       <CodeHighlighter :code='code' />
+
+      <div class="code-board-footer-info">
+
+      </div>
     </div>
 
 
@@ -34,6 +43,8 @@ export default {
       data:[],
       title:"",
       category:[],
+      userData:[],
+      registerDay:[],
     }
   },
   async created(){
@@ -43,16 +54,32 @@ export default {
       .then(res => {
         this.code = res.data[0].code
         this.data = res.data[0]
-        this.category = res.data[0].free_write.split('/')
-        console.log(this.category)
+        this.category = res.data[0].free_write.split('/')   
+        this.registerDay.push(this.data.register_date.split('T')[0])
+        console.log(this.data.register_date)
+        this.registerDay.push(this.data.register_date.split('T')[1].split('+')[0])     
+        console.log(this.data.register_date.split('T')[1].split('+')[0])
       })
       .catch(err => {
         console.log(err)
       })
 
+    if (this.data.is_active && localStorage.getItem('userNo')!==this.data.user_seq) {
+      alert('비공개게시물입니다.')
+      this.$router.push({name:'Main',params:{nickname:localStorage.getItem('email')}})
+    }
+
     await axios.get(`${SERVER_URL}/apps/v1/Problem/${this.data.problem_seq}`)
       .then(res=>{
         this.title = `${res.data.number}. ${res.data.name}`
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+
+    await axios.get(`${SERVER_URL}/apps/v1/userInfo/${this.data.user_seq}`)
+      .then(res =>{
+        this.userData = res.data
       })
       .catch(err=>{
         console.log(err)
@@ -65,12 +92,17 @@ export default {
 </script>
 
 <style>
-.code-board-body {  
+.code-board-content {  
   width:80%;
   margin: 0 auto;
 }
 
 .code-board-detail-status {
+  height: 40px;
+  /* background-color: red; */
+}
+
+.code-board-footer-info{
   height: 40px;
   background-color: red;
 }
