6269648364b3ea7af8230c0c37ecadd2a22bb735
pyoki32 <pyoki32@gmail.com>
2021-04-05 19:59:50 +0900

    Domain 경로 변경
    Former-commit-id: ecca33f054a9d126fc7d1f54c440f155e9688e70

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -16,7 +16,8 @@ import my_settings
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#DOMAIN = 'localhost:8000'
+DOMAIN = 'j4a302.p.ssafy.io'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
