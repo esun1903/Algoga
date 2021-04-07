0ba3b3c389b66fdf6e2228791212d74aab251c8e
이동희 <ldh29768@gmail.com>
2021-04-07 16:00:35 +0900

    savePoint
    Former-commit-id: ac018b18979572eecddb9a9a8e51f123a834a09c

--- frontend/algoga-frontend/src/components/Follow/Following.vue
+++ frontend/algoga-frontend/src/components/Follow/Following.vue
@@ -2,6 +2,7 @@
   <div id="follow-comp">
     <div class="person-div" v-for="(person,idx) in followingList" :key="idx">
       {{person}}
+      <img :src="profileImage(person)" alt="" height='100px' width='100px'>
 
     </div>
   </div>
@@ -12,7 +13,18 @@ export default {
   name:'Following',
   props:{
     followingList:Array,
-  }
+},
+  computed:{
+    profileImage(){
+      return (person) => {
+        console.log(person)
+        console.log(`require("${person.profile_image}")`)
+        
+        
+        return `'require("${person.profile_image}")'`
+      }
+    }
+  },
 }
 </script>
 
